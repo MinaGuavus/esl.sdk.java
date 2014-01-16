3d9d9341bfc18578c88e24c2b2ddee95475c5619
David Lawson <david_lawson@silanis.com>
2014-01-16 18:00:03 -0500

    Version 10.1.2.  FieldValidatorConverter now handles null members properly.  Incorporated Guava for using Optional<T>.

--- sdk/src/main/java/com/silanis/esl/sdk/internal/converter/FieldValidatorConverter.java
+++ sdk/src/main/java/com/silanis/esl/sdk/internal/converter/FieldValidatorConverter.java
@@ -1,5 +1,6 @@
 package com.silanis.esl.sdk.internal.converter;
 
+import com.google.common.base.Optional;
 import com.silanis.esl.api.model.FieldValidation;
 import com.silanis.esl.sdk.FieldValidator;
 
@@ -9,8 +10,8 @@ import java.util.ArrayList;
  * Converter between SDK FieldValidator and API FieldValidation.
  */
 public class FieldValidatorConverter {
-    private FieldValidator fieldValidator = null;
-    private FieldValidation fieldValidation = null;
+    private Optional<FieldValidator> fieldValidatorOptional;
+    private Optional<FieldValidation> fieldValidationOptional;
 
     /**
      * Construct with SDK object involved in conversion.
@@ -18,7 +19,8 @@ public class FieldValidatorConverter {
      * @param fieldValidator
      */
     public FieldValidatorConverter( FieldValidator fieldValidator ) {
-        this.fieldValidator = fieldValidator;
+        this.fieldValidatorOptional = Optional.of( fieldValidator );
+        this.fieldValidationOptional = Optional.absent();
     }
 
     /**
@@ -27,7 +29,8 @@ public class FieldValidatorConverter {
      * @param fieldValidation
      */
     public FieldValidatorConverter(FieldValidation fieldValidation) {
-        this.fieldValidation = fieldValidation;
+        this.fieldValidatorOptional = Optional.absent();
+        this.fieldValidationOptional = Optional.of( fieldValidation );
     }
 
     /**
@@ -36,31 +39,31 @@ public class FieldValidatorConverter {
      * @return a FieldValidation object.
      */
     public FieldValidation toAPIFieldValidation() {
-        if (fieldValidator == null) {
-            return fieldValidation;
+        if (fieldValidationOptional.isPresent()) {
+            return fieldValidationOptional.get();
         }
         FieldValidation fieldValidation = new FieldValidation();
 
-        if ( fieldValidator.getMaxLength() != null ) {
-            fieldValidation.setMaxLength( fieldValidator.getMaxLength() );
+        if ( fieldValidatorOptional.get().getMaxLength() != null ) {
+            fieldValidation.setMaxLength( fieldValidatorOptional.get().getMaxLength() );
         }
 
-        if ( fieldValidator.getMinLength() != null ) {
-            fieldValidation.setMinLength( fieldValidator.getMinLength() );
+        if ( fieldValidatorOptional.get().getMinLength() != null ) {
+            fieldValidation.setMinLength( fieldValidatorOptional.get().getMinLength() );
         }
 
-        if ( !fieldValidator.getOptions().isEmpty() ) {
+        if ( !fieldValidatorOptional.get().getOptions().isEmpty() ) {
             fieldValidation.setEnum( new ArrayList<String>() );
-            fieldValidation.getEnum().addAll( fieldValidator.getOptions() );
+            fieldValidation.getEnum().addAll( fieldValidatorOptional.get().getOptions() );
         }
 
-        fieldValidation.setRequired( fieldValidator.isRequired() );
-        if ( fieldValidator.getErrorMessage() != null ) {
-            fieldValidation.setErrorMessage( fieldValidator.getErrorMessage() );
+        fieldValidation.setRequired( fieldValidatorOptional.get().isRequired() );
+        if ( fieldValidatorOptional.get().getErrorMessage() != null ) {
+            fieldValidation.setErrorMessage( fieldValidatorOptional.get().getErrorMessage() );
         }
 
-        if ( fieldValidator.getRegex() != null ) {
-            fieldValidation.setPattern( fieldValidator.getRegex() );
+        if ( fieldValidatorOptional.get().getRegex() != null ) {
+            fieldValidation.setPattern( fieldValidatorOptional.get().getRegex() );
         }
 
         return fieldValidation;
@@ -72,16 +75,21 @@ public class FieldValidatorConverter {
      * @return a FieldValidator object.
      */
     public FieldValidator toSDKFieldValidator() {
-        if (fieldValidation == null) {
-            return fieldValidator;
+        if (fieldValidatorOptional.isPresent()) {
+            return fieldValidatorOptional.get();
         }
         FieldValidator fieldValidator = new FieldValidator();
-        fieldValidator.setErrorMessage(fieldValidation.getErrorMessage());
-        fieldValidator.setMaxLength(fieldValidation.getMaxLength());
-        fieldValidator.setMinLength(fieldValidation.getMinLength());
-        fieldValidator.setRegex(fieldValidation.getPattern());
-        fieldValidator.setRequired(fieldValidation.getRequired());
+        fieldValidator.setErrorMessage(fieldValidationOptional.get().getErrorMessage());
+
+        if ( fieldValidationOptional.get().getMaxLength() != null )
+            fieldValidator.setMaxLength(fieldValidationOptional.get().getMaxLength());
+
+        if ( fieldValidationOptional.get().getMinLength() != null )
+            fieldValidator.setMinLength(fieldValidationOptional.get().getMinLength());
+
+        fieldValidator.setRegex(fieldValidationOptional.get().getPattern());
+        fieldValidator.setRequired(fieldValidationOptional.get().evalRequired());
+
         return fieldValidator;
     }
-
 }
